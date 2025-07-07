31fc37034352104a039f21168d07d4d2cfee3c7a
mehmet.afacan <mehmet.afacan@tubitak.gov.tr>
2025-07-07 08:38:06 +0300

    Doğum günü kalan gün hesaplama hatası düzeltildi.

--- YTE-BOOTCAMP/src/main/java/practice_3/DogumGunuTebrikProgrami.java
+++ YTE-BOOTCAMP/src/main/java/practice_3/DogumGunuTebrikProgrami.java
@@ -1,6 +1,6 @@
 package practice_3;
 
-import java.time.LocalDateTime;
+import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoUnit;
 import java.util.Scanner;
@@ -11,21 +11,25 @@ public class DogumGunuTebrikProgrami {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("--- Doğum Günü Tebrik Programı ---");
-        System.out.print("Doğum gününüzü ve saatinizi girin (örn. 2000-09-15 13:30): ");
+        System.out.print("Doğum gününüzü ve saatinizi girin (örn. 2000-09-15): ");
         String dogumGunuInput = scanner.nextLine();
 
-
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
-        LocalDateTime dogumGunu = LocalDateTime.parse(dogumGunuInput, formatter);
-
-        LocalDateTime bugun = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        LocalDate dogumGunu = LocalDate.parse(dogumGunuInput, formatter);
+        LocalDate bugun = LocalDate.now();
 
         if (dogumGunu.getMonthValue() == bugun.getMonthValue() && dogumGunu.getDayOfMonth() == bugun.getDayOfMonth()) {
             System.out.println("Bugün doğum gününüz! İyi ki doğdunuz! 🎉🎂");
         } else {
-            long gunKaldi = bugun.until(dogumGunu, ChronoUnit.DAYS);
-            long saatkaldi = bugun.until(dogumGunu, ChronoUnit.HOURS);
-            System.out.println("Doğum gününüze " + gunKaldi + " gün, veya " + saatkaldi + " saat " + "kaldı. 🎉🎂");
+            long kalanGun;
+            boolean isDogumGunuGecmis = bugun.getDayOfYear() - dogumGunu.getDayOfYear() >= 1;
+            if (isDogumGunuGecmis) {
+                LocalDate dogumGunuBuYil = dogumGunu.plusYears(bugun.getYear() - dogumGunu.getYear());
+                kalanGun = ((bugun.until(dogumGunuBuYil, ChronoUnit.DAYS) + 365));
+            } else {
+                kalanGun = dogumGunu.getDayOfYear() - bugun.getDayOfYear() - 1;
+            }
+            System.out.println("Doğum gününüze " + kalanGun + " gün " + "kaldı. 🎉🎂");
         }
         scanner.close();
     }
